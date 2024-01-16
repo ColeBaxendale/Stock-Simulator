9b95ede15d384bb388d7d9b3d86832a3507641ba
BasicallyClutch <97856451+ColeBaxendale@users.noreply.github.com>
2024-01-15 19:28:53 -0500

    first commit, added login and register auth

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/users');
+
+
+
+const app = express();
+
+// Mongo Connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+// Middleware for parsing JSON and URL-encoded data.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/users', userRoutes);
+
+// Route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
